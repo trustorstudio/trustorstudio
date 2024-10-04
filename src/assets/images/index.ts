6647400c5bb5be589143337570a9289e112d6918
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-10-04 22:42:14 +0700

    develop: create and update 404 notfound page

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,7 @@
 const images = {
     logo: require("./logo.svg"),
     subcribe: require("./subcribe.gif"),
-
+    notFound: require("./404.png"),
     // social media begin
     x: require("./social-medias/x.svg"),
     // social media end
